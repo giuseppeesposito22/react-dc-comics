e6451972f754cdaccf3e88261c10e8f1317745bf
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-20 13:48:00 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/components/header";
+import Main from "./assets/components/Main";
+import Footer from "./assets/components/Footer";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header id="page-header">
+      <img src="\img\dc-logo-bg.png" alt="" />
+    </header>
+  );
+}
