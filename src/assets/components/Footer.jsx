33d7511cfabaa579f9d8d6fbc07867e73bd1ac6d
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-20 15:42:35 +0200

    added footer link

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,7 +1,121 @@
 export default function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer id="page-footer">
+      <section className="footer-link">
+        <div className="container">
+          <div className="col-1-2">
+            <div className="col-1-5">
+              <h3>DC</h3>
+              <ul>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+              </ul>
+              <h3>DC</h3>
+              <ul>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1-5">
+              <h3>DC</h3>
+              <ul>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1-5">
+              <h3>DC</h3>
+              <ul>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="footer-social"></section>
     </footer>
   );
 }
