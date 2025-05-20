ae6130d5ce7e8fc8bd8d10f5eb341e681b621b57
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-20 20:13:22 +0200

    fix import Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./assets/components/header";
+import Header from "./assets/components/Header";
 import Main from "./assets/components/Main";
 import Footer from "./assets/components/Footer";
 
