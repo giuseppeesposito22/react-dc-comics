e8e490a6bd897ea7c4f32c7dc5c76d3effcda4cf
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-20 14:54:31 +0200

    added main

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,8 +1,43 @@
 export default function Main() {
   return (
-    <main>
-      <section className="content-section"></section>
-      <section className="shop-section"></section>
+    <main id="page-main">
+      <section className="content-section">
+        <div className="container">Content goes here</div>
+      </section>
+      <section className="shop-section">
+        <div className="container">
+          <div className="col-1-5">
+            <div className="main-shop">
+              <img src="\img\buy-comics-digital-comics.png" alt="" />
+              <p>DIGITAL COMICS</p>
+            </div>
+          </div>
+          <div className="col-1-5">
+            <div className="main-shop">
+              <img src="\img\buy-comics-merchandise.png" alt="" />
+              <p>DC MERCHANDISE</p>
+            </div>
+          </div>
+          <div className="col-1-5">
+            <div className="main-shop">
+              <img src="\img\buy-comics-subscriptions.png" alt="" />
+              <p>SUBSCRIPTION</p>
+            </div>
+          </div>
+          <div className="col-1-5">
+            <div className="main-shop">
+              <img src="\img\buy-comics-shop-locator.png" alt="" />
+              <p>COMIC SHOP LOCATOR</p>
+            </div>
+          </div>
+          <div className="col-1-5">
+            <div className="main-shop">
+              <img src="\img\buy-dc-power-visa.svg" alt="" />
+              <p>DC POWER VISA</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
