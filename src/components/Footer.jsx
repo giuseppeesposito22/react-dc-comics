1d1124837d0f3e25139c8027c3c253924466cbfe
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-21 17:09:40 +0200

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div className="container">
           <div className="col-1-2">
             <div className="col-1-5">
-              <h3>DC</h3>
+              <h3>dc comics</h3>
               <ul>
                 <li>
                   link<a href="#"></a>
@@ -29,58 +29,8 @@ export default function Footer() {
                   link<a href="#"></a>
                 </li>
               </ul>
-              <h3>DC</h3>
+              <h3>shop</h3>
               <ul>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-1-5">
-              <h3>DC</h3>
-              <ul>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
-                <li>
-                  link<a href="#"></a>
-                </li>
                 <li>
                   link<a href="#"></a>
                 </li>
@@ -93,7 +43,57 @@ export default function Footer() {
               </ul>
             </div>
             <div className="col-1-5">
-              <h3>DC</h3>
+              <h3>dc</h3>
+              <ul>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+                <li>
+                  link<a href="#"></a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-1-5">
+              <h3>sites</h3>
               <ul>
                 <li>
                   link<a href="#"></a>
@@ -123,10 +123,10 @@ export default function Footer() {
           <div className="follow-us">
             <p>follow us</p>
             <img src="\img\footer-facebook.png" alt="" />
-            <img src="\img\footer-facebook.png" alt="" />
-            <img src="\img\footer-facebook.png" alt="" />
-            <img src="\img\footer-facebook.png" alt="" />
-            <img src="\img\footer-facebook.png" alt="" />
+            <img src="\img\footer-twitter.png" alt="" />
+            <img src="\img\footer-youtube.png" alt="" />
+            <img src="\img\footer-pinterest.png" alt="" />
+            <img src="\img\footer-periscope.png" alt="" />
           </div>
         </div>
       </section>
